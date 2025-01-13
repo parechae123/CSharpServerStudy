99f7e201b2419023c12b1dd142627695d8726ac9
heoyoon <parechae@gmail.com>
2025-01-14 03:13:53 +0900

    feat : 로그인,채팅 캡슐화, broadCast함수 추가

--- Server/Network/MultiConnect.cs
+++ Server/Network/MultiConnect.cs
@@ -5,13 +5,16 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MessagePack.Formatters;
+using System.Text.Json.Serialization;
 
 namespace CSharpServerStudy.Server.Network
 {
     internal class MultiConnect
     {
         private readonly int _port;
-        private Dictionary<string,NetworkStream> users = new Dictionary<string, NetworkStream>();
+        private Dictionary<string,TcpClient> users = new Dictionary<string, TcpClient>();
         public MultiConnect(int port)
         {
             _port = port;
@@ -28,7 +31,6 @@ namespace CSharpServerStudy.Server.Network
                 // 비동기적으로 클라이언트 접속 대기
                 var client = await listener.AcceptTcpClientAsync();
                 Console.WriteLine("클라이언트 연결됨!");
-
                 // 클라이언트 처리 작업을 백그라운드에서 실행
                 //_= : 반환값을 무시(저장안)한다는걸 명시적으로 표현
                 _ = Task.Run(() => HandleClientAsync(client));
@@ -39,53 +41,13 @@ namespace CSharpServerStudy.Server.Network
         {
             //try 도중 오류발생 => catch => finally 실행
             //오류 x try => finally
+            string userName = string.Empty;
             try
             {
                 var stream = client.GetStream();
                 var buffer = new byte[1024];
-                string userName = string.Empty;
-                while (true)
-                {
-                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-
-                    // 받은 데이터 처리
-                    userName = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    if (users.ContainsKey(userName))
-                    {
-                        _ = stream.WriteAsync(new byte[1]{0}, 0, 1);
-                    }
-                    else
-                    {
-                        users.Add(userName,stream);
-                        _ = stream.WriteAsync(new byte[1] { 1 }, 0, 1);
-                        break;
-                    }
-                }
-
-                // 클라이언트와의 통신 루프
-                while (true)
-                {
-                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-
-                    // 받은 데이터 처리
-                    var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine($"{userName} : {message}");
-
-                    // Echo back (받은 데이터를 다시 클라이언트로 전송)
-                    var response = Encoding.UTF8.GetBytes($"{userName} : {message}");
-                    foreach ( var item in users )
-                    {
-                        if (item.Key != userName)
-                        {
-                            await item.Value.WriteAsync(response, 0, response.Length);
-                        }
-                        else continue;
-                    }
-                    if (message[0] == '.')
-                    {
-                        break;
-                    }
-                }
+                await Login(buffer,stream,client);
+                await Chatting(buffer,stream);
             }
             catch (Exception ex)
             {
@@ -94,7 +56,52 @@ namespace CSharpServerStudy.Server.Network
             finally
             {
                 Console.WriteLine("클라이언트 연결 종료");
+                users.Remove(userName);
                 client.Close();
+            }
+        }
+
+        private async Task Login(byte[] buffer, NetworkStream stream,TcpClient client)
+        {
+            while (true)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                // 받은 데이터 처리
+                string userName = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                if (users.ContainsKey(userName))
+                {
+                    _ = stream.WriteAsync(new byte[1] { 0 }, 0, 1);
+                }
+                else
+                {
+                    users.Add(userName, client);
+                    _ = stream.WriteAsync(new byte[1] { 1 }, 0, 1);
+                    break;
+                }
+            }
+        }
+        private async Task Chatting(byte[] buffer, NetworkStream stream)
+        {
+            // 클라이언트와의 통신 루프
+            while (true)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                // 받은 데이터 처리
+                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                //name으로 단순화 시켰으나 추후에 객체를 json화 시켜서 주고받고 Read한 값을 json으로 옮겨서 넣으면 될듯?
+                Console.WriteLine(message);
+                // Echo back (받은 데이터를 다시 클라이언트로 전송)
+                var response = Encoding.UTF8.GetBytes(message);
+                BroadCast(response);
+            }
+        }
+
+        private void BroadCast(byte[] buffer)
+        {
+            foreach (var item in users)
+            {
+                item.Value.GetStream().Write(buffer, 0, buffer.Length);
             }
         }
     }
